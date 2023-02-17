7551c80e653648d4e01f15574a1814586556bc3d
Taras Sheketa <taras.sheketa@gmail.com>
2023-02-17 20:58:41 +0200

    hw6

--- app/currency/models.py
+++ app/currency/models.py
@@ -2,8 +2,8 @@ from django.db import models
 
 
 class Rate(models.Model):
-    created = models.DateTimeField(auto_now_add=True)
+    created =models.DateTimeField(auto_now_add=True)
     currency = models.CharField(max_length=25)  # usd, eur
     buy = models.DecimalField(max_digits=6, decimal_places=2)
-    sell = models.DecimalField(max_digits=6, decimal_places=2)
+    sell= models.DecimalField(max_digits=6, decimal_places=2)
     source = models.CharField(max_length=25)
