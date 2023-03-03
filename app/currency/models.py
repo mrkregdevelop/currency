af225e30cad206eee105d3ef9d4bf3195065c82b
Taras Sheketa <taras.sheketa@gmail.com>
2023-03-03 21:31:12 +0200

    Lesson 10

--- app/currency/models.py
+++ app/currency/models.py
@@ -1,12 +1,20 @@
 from django.db import models
 
+from currency.choices import RateCurrencyChoices
+
 
 class Rate(models.Model):
     created = models.DateTimeField(auto_now_add=True)
-    currency = models.CharField(max_length=25)  # usd, eur
+    currency = models.PositiveSmallIntegerField(
+        choices=RateCurrencyChoices.choices,
+        default=RateCurrencyChoices.USD
+    )  # if field has choices - get_{field_name}_display(), get_currency_display()
     buy = models.DecimalField(max_digits=6, decimal_places=2)
     sell = models.DecimalField(max_digits=6, decimal_places=2)
     source = models.CharField(max_length=25)
+
+    def __str__(self):
+        return f'Currency: {self.get_currency_display()}, Buy: {self.buy}'
 
 
 class Source(models.Model):
